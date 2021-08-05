eab11eff98df0edf2b4fffaece22004b077444a5
Gonzalo Fleming <gonzalo.fleming@digitalbeat.cl>
2021-08-05 00:40:57 -0400

    Modificación de altura mínima

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,91 +1,122 @@
 <template>
-  <div class="home">
-    <section>
-      <b-container fluid>
-        <b-row>
-          <b-col cols="12" md="6" class="p-0 img-b d-flex justify-content-center align-items-center">
-            <div class="img-woman"></div>
-          </b-col>
-          <b-col cols="12" md="6" class="d-flex align-items-center log-t-i">
-            <b-container>
-              <b-row>
-                <b-col cols="12">
-                  <div class="logo-text d-flex flex-column align-items-center mx-auto">
-                    <img src="../assets/logo.png">
-                    <h1 class="text-uppercase mt-3 text-lg-right text-center">Accesorios + servicio técnico para dispositivos móviles</h1>
+  <b-container class="alto-min" fluid>
+    <b-row class="alto-min">
+      <b-col cols="12" md="6" class="p-0 alto-min">
+        <div class="img-woman"></div>
+      </b-col>
+
+      <b-col cols="12" md="6" class="d-flex align-items-center log-t-i alto-min">
+        <b-container>
+          <b-row>
+            <b-col cols="12">
+              <div
+                class="logo-text d-flex flex-column align-items-center mx-auto"
+              >
+                <img src="../assets/logo.png" />
+                <h1 class="text-uppercase mt-3 text-lg-right text-center">
+                  Accesorios + servicio técnico para dispositivos móviles
+                </h1>
+              </div>
+            </b-col>
+            <b-col cols="12">
+              <h2 class="py-4 text-center">
+                Buscamos proteger, mejorar y prolongar la vida de cualquier
+                dispositivo móvil
+              </h2>
+            </b-col>
+            <b-row class="pt-4 no-gutters">
+              <b-col cols="12" md="6">
+                <a
+                  class="d-flex btn-rrss"
+                  href="https://www.lifemaxstore.cl"
+                  target="_blank"
+                >
+                  <div class="btn-rrss-icon ov-btn-grow-skew">Sitio Web</div>
+                  <div class="iconos"><i class="fas fa-globe"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="6">
+                <a
+                  class="d-flex btn-rrss"
+                  href="https://wa.me/56966263774"
+                  target="_blank"
+                >
+                  <div class="btn-rrss-icon ov-btn-grow-skew">
+                    Ventas Detalle
                   </div>
-                </b-col>
-                <b-col cols="12">
-                  <h2 class="py-4 text-center">Buscamos proteger, mejorar y prolongar la vida de cualquier dispositivo móvil</h2>
-                </b-col>
-                <b-row class="pt-4 no-gutters">
-                  <b-col cols="12" md="6">
-                    <a class="d-flex btn-rrss" href="https://www.lifemaxstore.cl" target="_blank">
-                      <div class="btn-rrss-icon ov-btn-grow-skew">Sitio Web</div>
-                      <div class="iconos"><i class="fas fa-globe"></i></div>
-                    </a>
-                  </b-col>
-                  <b-col cols="12" md="6">
-                    <a class="d-flex btn-rrss" href="https://wa.me/56966263774" target="_blank">
-                      <div class="btn-rrss-icon ov-btn-grow-skew">Ventas Detalle</div>
-                      <div class="iconos"><i class="fab fa-whatsapp"></i></div>
-                    </a>
-                  </b-col>
-                  <b-col cols="12" md="6">
-                    <a class="d-flex btn-rrss" href="https://wa.me/message/ZJ6JGUXRFXXRP1" target="_blank">
-                      <div class="btn-rrss-icon ov-btn-grow-skew">Ventas Mayor</div>
-                      <div class="iconos"><i class="fab fa-whatsapp"></i></div>
-                    </a>
-                  </b-col>
-                  <b-col cols="12" md="6">
-                    <a class="d-flex btn-rrss" href="https://www.facebook.com/lifemax.cll/" target="_blank">
-                      <div class="btn-rrss-icon ov-btn-grow-skew">Facebook</div>
-                      <div class="iconos"><i class="fab fa-facebook-f"></i></div>
-                    </a>
-                  </b-col>
-                  <b-col cols="12" md="6">
-                    <a class="d-flex btn-rrss" href="https://www.instagram.com/lifemax.cl/?igshid=8i55gwoo9do2" target="_blank">
-                      <div class="btn-rrss-icon ov-btn-grow-skew">Instagram</div>
-                      <div class="iconos"><i class="fab fa-instagram"></i></div>
-                    </a>
-                  </b-col>
-                   <b-col cols="12" md="6">
-                    <a class="d-flex btn-rrss" href="#">
-                      <div class="btn-rrss-icon ov-btn-grow-skew">TikTok</div>
-                      <div class="iconos"><i class="fab fa-tiktok"></i></div>
-                    </a>
-                  </b-col>
-                   <b-col cols="12" md="12">
-                    <a class="d-flex btn-rrss" href="mailto:ventas@lifemaxstore.cl">
-                      <div class="btn-rrss-icon ov-btn-grow-skew">Email</div>
-                      <div class="iconos"><i class="far fa-envelope"></i></div>
-                    </a>
-                  </b-col>
-                </b-row>
-              </b-row>
-              <b-row>
-                <b-col cols="12">
-                  <div class="d-flex justify-content-center align-items-center">
-                    <i class="fas fa-map-marker-alt"></i>
-                    <a href="https://www.google.com/maps/place/LifeMax+-+Accesorios+%2B+Servicio+Tecnico+Celulares/@-33.4466041,-70.6529295,17z/data=!3m1!4b1!4m5!3m4!1s0x9662c5e0bff76e9f:0xae2f1f634db6247f!8m2!3d-33.4466033!4d-70.650731" target="_blank" class="ms-2">Santiago de Chile</a>
-                  </div>
-                </b-col>
-                <b-col cols="12">
-                  <div class="text-center pt-3">Copyright © 2019. Todos los derechos reservados</div>
-                </b-col>
-              </b-row>
-            </b-container>
-          </b-col>
-        </b-row>
-      </b-container>
-    </section>
-  </div>
+                  <div class="iconos"><i class="fab fa-whatsapp"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="6">
+                <a
+                  class="d-flex btn-rrss"
+                  href="https://wa.me/message/ZJ6JGUXRFXXRP1"
+                  target="_blank"
+                >
+                  <div class="btn-rrss-icon ov-btn-grow-skew">Ventas Mayor</div>
+                  <div class="iconos"><i class="fab fa-whatsapp"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="6">
+                <a
+                  class="d-flex btn-rrss"
+                  href="https://www.facebook.com/lifemax.cll/"
+                  target="_blank"
+                >
+                  <div class="btn-rrss-icon ov-btn-grow-skew">Facebook</div>
+                  <div class="iconos"><i class="fab fa-facebook-f"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="6">
+                <a
+                  class="d-flex btn-rrss"
+                  href="https://www.instagram.com/lifemax.cl/?igshid=8i55gwoo9do2"
+                  target="_blank"
+                >
+                  <div class="btn-rrss-icon ov-btn-grow-skew">Instagram</div>
+                  <div class="iconos"><i class="fab fa-instagram"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="6">
+                <a class="d-flex btn-rrss" href="#">
+                  <div class="btn-rrss-icon ov-btn-grow-skew">TikTok</div>
+                  <div class="iconos"><i class="fab fa-tiktok"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="12">
+                <a class="d-flex btn-rrss" href="mailto:ventas@lifemaxstore.cl">
+                  <div class="btn-rrss-icon ov-btn-grow-skew">Email</div>
+                  <div class="iconos"><i class="far fa-envelope"></i></div>
+                </a>
+              </b-col>
+            </b-row>
+          </b-row>
+          <b-row>
+            <b-col cols="12">
+              <div class="d-flex justify-content-center align-items-center">
+                <i class="fas fa-map-marker-alt"></i>
+                <a
+                  href="https://www.google.com/maps/place/LifeMax+-+Accesorios+%2B+Servicio+Tecnico+Celulares/@-33.4466041,-70.6529295,17z/data=!3m1!4b1!4m5!3m4!1s0x9662c5e0bff76e9f:0xae2f1f634db6247f!8m2!3d-33.4466033!4d-70.650731"
+                  target="_blank"
+                  class="ms-2"
+                  >Santiago de Chile</a
+                >
+              </div>
+            </b-col>
+            <b-col cols="12">
+              <div class="text-center pt-3">
+                Copyright © 2019. Todos los derechos reservados
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-}
+  name: "Home",
+};
 </script>
-
-
