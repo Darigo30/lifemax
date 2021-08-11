269d4bd36a46033c99c976695b7222e1f75c85a3
darigo30 <darigomez30@gmail.com>
2021-08-10 23:22:45 -0400

    agregado de botones y modales

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,8 +10,7 @@
           <b-row>
             <b-col cols="12">
               <div
-                class="logo-text d-flex flex-column align-items-center mx-auto"
-              >
+                class="logo-text d-flex flex-column align-items-center mx-auto">
                 <img src="../assets/logo.png" />
                 <h1 class="text-uppercase mt-3 text-lg-right text-center">
                   Accesorios + servicio técnico para dispositivos móviles
@@ -26,53 +25,31 @@
             </b-col>
             <b-row class="pt-4 no-gutters">
               <b-col cols="12" md="6">
-                <a
-                  class="d-flex btn-rrss"
-                  href="https://www.lifemaxstore.cl"
-                  target="_blank"
-                >
+                <a class="d-flex btn-rrss" v-b-modal.modal-center>
                   <div class="btn-rrss-icon ov-btn-grow-skew">Sitio Web</div>
                   <div class="iconos"><i class="fas fa-globe"></i></div>
                 </a>
               </b-col>
               <b-col cols="12" md="6">
-                <a
-                  class="d-flex btn-rrss"
-                  href="https://wa.me/56966263774"
-                  target="_blank"
-                >
-                  <div class="btn-rrss-icon ov-btn-grow-skew">
-                    Ventas Detalle
-                  </div>
+                <a class="d-flex btn-rrss" v-b-modal.modal-whats>
+                  <div class="btn-rrss-icon ov-btn-grow-skew">WhatsApp</div>
                   <div class="iconos"><i class="fab fa-whatsapp"></i></div>
                 </a>
               </b-col>
               <b-col cols="12" md="6">
-                <a
-                  class="d-flex btn-rrss"
-                  href="https://wa.me/message/ZJ6JGUXRFXXRP1"
-                  target="_blank"
-                >
-                  <div class="btn-rrss-icon ov-btn-grow-skew">Ventas Mayor</div>
-                  <div class="iconos"><i class="fab fa-whatsapp"></i></div>
+                <a class="d-flex btn-rrss" v-b-modal.modal-surc>
+                  <div class="btn-rrss-icon ov-btn-grow-skew">Sucursales</div>
+                  <div class="iconos"><i class="fas fa-map-marked-alt"></i></div>
                 </a>
               </b-col>
               <b-col cols="12" md="6">
-                <a
-                  class="d-flex btn-rrss"
-                  href="https://www.facebook.com/lifemax.cll/"
-                  target="_blank"
-                >
+                <a class="d-flex btn-rrss" href="https://www.facebook.com/lifemax.cll/" target="_blank">
                   <div class="btn-rrss-icon ov-btn-grow-skew">Facebook</div>
                   <div class="iconos"><i class="fab fa-facebook-f"></i></div>
                 </a>
               </b-col>
               <b-col cols="12" md="6">
-                <a
-                  class="d-flex btn-rrss"
-                  href="https://www.instagram.com/lifemax.cl/?igshid=8i55gwoo9do2"
-                  target="_blank"
-                >
+                <a class="d-flex btn-rrss" href="https://www.instagram.com/lifemax.cl/?igshid=8i55gwoo9do2" target="_blank">
                   <div class="btn-rrss-icon ov-btn-grow-skew">Instagram</div>
                   <div class="iconos"><i class="fab fa-instagram"></i></div>
                 </a>
@@ -83,10 +60,34 @@
                   <div class="iconos"><i class="fab fa-tiktok"></i></div>
                 </a>
               </b-col>
-              <b-col cols="12" md="12">
+              <b-col cols="12" md="6">
+                <a class="d-flex btn-rrss" href="https://eshops.mercadolibre.cl/lifemax" target="_blank">
+                  <div class="btn-rrss-icon ov-btn-grow-skew">Mercado Libre</div>
+                  <div class="iconos"><i class="fas fa-store"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="6">
+                <a class="d-flex btn-rrss" href="https://www.linio.cl/s/lifemax-" target="_blank">
+                  <div class="btn-rrss-icon ov-btn-grow-skew">Linio</div>
+                  <div class="iconos"><i class="fas fa-store"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="6">
+                <a class="d-flex btn-rrss" href="https://lifemax.mercadoshops.cl/" target="_blank">
+                  <div class="btn-rrss-icon ov-btn-grow-skew">MercadoShop</div>
+                  <div class="iconos"><i class="fas fa-store"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="6">
                 <a class="d-flex btn-rrss" href="mailto:ventas@lifemaxstore.cl">
                   <div class="btn-rrss-icon ov-btn-grow-skew">Email</div>
                   <div class="iconos"><i class="far fa-envelope"></i></div>
+                </a>
+              </b-col>
+              <b-col cols="12" md="12">
+                <a class="d-flex btn-rrss" v-b-modal.modal-catal>
+                  <div class="btn-rrss-icon ov-btn-grow-skew">Catálogo de Productos</div>
+                  <div class="iconos"><i class="fas fa-file-pdf"></i></div>
                 </a>
               </b-col>
             </b-row>
@@ -112,6 +113,63 @@
         </b-container>
       </b-col>
     </b-row>
+    <!-- Modales-->
+    <!-- Modal Sitios Web-->
+    <b-modal id="modal-center" v-bind:hide-footer="true" class="titlemodal" centered title="Sitios Web">
+      <div class="my-4">
+        <a class="d-flex btn-rrss-modal" href="https://www.lifemaxstore.cl" target="_blank">
+          <div class="btn-rrss-icon ov-btn-grow-skew">Ventas Detalle</div>
+          <div class="iconos"><i class="fas fa-globe"></i></div>
+        </a>
+        <a class="d-flex btn-rrss-modal" href="https://www.lifemaxdistribuidora.cl" target="_blank">
+          <div class="btn-rrss-icon ov-btn-grow-skew">Ventas Mayoristas</div>
+          <div class="iconos"><i class="fas fa-globe"></i></div>
+        </a>
+      </div>
+    </b-modal>
+    <!-- Modal Whatsapp-->
+    <b-modal id="modal-whats" v-bind:hide-footer="true" class="titlemodal" centered title="WhatsApp">
+      <div class="my-4">
+        <a class="d-flex btn-rrss-modal" href="https://wa.me/56966263774" target="_blank">
+          <div class="btn-rrss-icon ov-btn-grow-skew">Ventas Detalle</div>
+          <div class="iconos"><i class="fab fa-whatsapp"></i></div>
+        </a>
+        <a class="d-flex btn-rrss-modal" href="https://wa.me/message/ZJ6JGUXRFXXRP1" target="_blank">
+          <div class="btn-rrss-icon ov-btn-grow-skew">Ventas Mayor</div>
+          <div class="iconos"><i class="fab fa-whatsapp"></i></div>
+        </a>
+      </div>
+    </b-modal>
+    <!-- Modal Sucursales-->
+    <b-modal id="modal-surc" v-bind:hide-footer="true" class="titlemodal" centered title="Sucursales">
+      <div class="my-4">
+        <a class="d-flex btn-rrss-modal" href="https://www.google.com/maps/place/LifeMax+-+Accesorios+%2B+Servicio+Tecnico+Celulares/@-33.4466041,-70.6529295,17z/data=!3m1!4b1!4m5!3m4!1s0x9662c5e0bff76e9f:0xae2f1f634db6247f!8m2!3d-33.4466033!4d-70.650731" target="_blank">
+          <div class="btn-rrss-icon ov-btn-grow-skew">Matriz</div>
+          <div class="iconos"><i class="fas fa-map-marked-alt"></i></div>
+        </a>
+        <a class="d-flex btn-rrss-modal" href="https://www.google.com/maps/place/LifeMax+-+Accesorios+%2B+Servicio+Tecnico+Celulares/@-33.4466041,-70.6529295,17z/data=!3m1!4b1!4m5!3m4!1s0x9662c5e0bff76e9f:0xae2f1f634db6247f!8m2!3d-33.4466033!4d-70.650731" target="_blank">
+          <div class="btn-rrss-icon ov-btn-grow-skew">Sucursal</div>
+          <div class="iconos"><i class="fas fa-map-marked-alt"></i></div>
+        </a>
+      </div>
+    </b-modal>
+    <!-- Modal Catálogo de productos-->
+    <b-modal id="modal-catal" v-bind:hide-footer="true" class="titlemodal" centered title="Catálogo de Productos">
+      <div class="my-4">
+        <a class="d-flex btn-rrss-modal" href="http://lifemax-store.cl/archivos/catalagodeRepuestoLifemax.pdf" target="_blank">
+          <div class="btn-rrss-icon ov-btn-grow-skew">Repuestos</div>
+          <div class="iconos"><i class="fas fa-file-pdf"></i></div>
+        </a>
+        <a class="d-flex btn-rrss-modal" href="http://lifemax-store.cl/archivos/catalogoaccesorioslifemax.pdf" target="_blank">
+          <div class="btn-rrss-icon ov-btn-grow-skew">Accesorios</div>
+          <div class="iconos"><i class="fas fa-file-pdf"></i></div>
+        </a>
+        <a class="d-flex btn-rrss-modal" href="http://lifemax-store.cl/archivos/cataalogo-computacion-lifemax.pdf" target="_blank">
+          <div class="btn-rrss-icon ov-btn-grow-skew">Computación</div>
+          <div class="iconos"><i class="fas fa-file-pdf"></i></div>
+        </a>
+      </div>
+    </b-modal>
   </b-container>
 </template>
 
